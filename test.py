1ed2c2d190153d94bd302319e389fdf3df389330
louisljamie <louis.l.jamie@gmail.com>
2022-09-21 21:21:52 -0500

    made the function better

--- test.py
+++ test.py
@@ -1,3 +1,13 @@
-print(hi mom)
 
 
+num = input('Whats the nums?: ')
+def is_positive_even(num):
+    if num > 0 and num % 2 == 0:
+        return True
+    else:
+        return False
+    
+    num = 3
+
+
+is_positive_even()
